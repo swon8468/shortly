a241ee667e5dc3ac00ee9ca9c9f4dd7d3310b9d5
swon-04 <swon8468@gmail.com>
2024-06-24 13:25:28 +0900

    join

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'), # 메인 화면
+    path('login/', views.login, name='login'), # 로그인
+    path('join', views.join, name='join'), # 회원가입
+    path('form_process', views.form_process, name='form_process'), # 폼 처리
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import CustomUser
+from home.models import CustomUser
+from django.http import HttpResponseRedirect
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
+    return render(request, 'index.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def join(request):
+    return render(request, 'join.html')
+
+def form_process(request):
+    if request.method == 'POST':
+        form_type = request.POST.get('form_type')
+        if form_type == 'join':
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+            password1 = request.POST.get('password1')
+            password2 = request.POST.get('password2')
+            if password1 != password2:
+                messages.error('Error!\nComfirm your Password and Re-password')
+                return redirect('join')
+            if password1 == password2:
+                if CustomUser.objects.filter(email=email).exists():
+                    messages.error('Error!\nThe email already exists')
+                else:
+                    user = CustomUser.objects.create_user(email=email, name=name, password=password1)
+                    user.save()
+                    messages.success('Success!\nLogin your account')
+                    return redirect('login')
+
     return render(request, 'index.html')
